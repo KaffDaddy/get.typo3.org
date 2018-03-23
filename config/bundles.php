9c571a495b6703f63f32638a2255410a7f046644
Susanne Moog <s.moog@neusta.de>
2018-03-23 16:01:12 +0100

    [TASK] CORS

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
